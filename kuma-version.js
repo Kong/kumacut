6aaf7a401200572dbf275ffb575ad1af54f64e3f
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-10-22 14:55:19 -0400

    created flags for all release types and streamlined things to make this act more like a standard cli tool. beginning workings of directory manipulation.

--- kuma-version.js
+++ kuma-version.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
+const { lstatSync, readdirSync } = require("fs");
+const { join } = require("path");
 const program = require("commander");
 const chalk = require("chalk");
+const LatestSemver = require("latest-semver");
 
 const namespace = "Kuma";
 const scriptVer = "0.0.1";
 
-const LatestSemver = require("latest-semver");
+const CURR_DIR = process.cwd();
 const releases = require("./docs/public/releases.json");
 const latest = LatestSemver(releases);
 
@@ -26,31 +29,83 @@ if (program.all) {
   console.log(releases);
 }
 
+/**
+ *
+ */
+isDirectory = source => {
+  lstatSync(source).isDirectory();
+};
+
+getDirectories = source => {
+  readdirSync(source)
+    .map(name => join(source, name))
+    .filter(isDirectory);
+};
+
+/**
+ * bumpVersion
+ */
+bumpVersion = (type, val) => {
+  let currentVer = latest.split(".");
+  let major = parseInt(currentVer[0]);
+  let minor = parseInt(currentVer[1]);
+  let patch = parseInt(currentVer[2]);
+  let label, ver;
+
+  switch (type) {
+    case "major":
+      ver = `${major + 1}.${minor}.${patch}`;
+      label = "major";
+      break;
+    case "minor":
+      ver = `${major}.${minor + 1}.${patch}`;
+      label = "minor";
+      break;
+    case "patch":
+      ver = `${major}.${minor}.${patch + 1}`;
+      label = "patch";
+      break;
+    case "custom":
+      ver = val.replace("v", "");
+      label = "custom";
+      break;
+  }
+
+  console.log(
+    `${chalk.green.bold("✔")} New Release: ${chalk.blue.bold(label)}, ${chalk.green.bold(latest)} ➜ ${chalk.green.bold(
+      ver
+    )}`
+  );
+
+  // return ver;
+};
+
 program
-  .command("cut [releaseType]")
-  .description("The type of release.")
-  .action(releaseType => {
-    let currentVer = latest.split(".");
-    let major = parseInt(currentVer[0]);
-    let minor = parseInt(currentVer[1]);
-    let patch = parseInt(currentVer[2]);
-    let newVer;
-    let newType = "patch";
-
-    if (releaseType === "major") {
-      newVer = `${major + 1}.${minor}.${patch}`;
-      newType = "major";
-    } else if (releaseType === "minor") {
-      newVer = `${major}.${minor + 1}.${patch}`;
-      newType = "minor";
-    } else {
-      newVer = `${major}.${minor}.${patch + 1}`;
-    }
-
-    console.log(`New version: ${chalk.blue.bold(newType)}, ${chalk.green.bold(latest)} ➜ ${chalk.green.bold(newVer)}`);
-
-    // return newVer;
-  });
+  .option("--major", "Cut a major release.")
+  .option("--minor", "Cut a minor release.")
+  .option("--patch", "Cut a patch release.")
+  .option("--custom <ver>", "Cut a release with a user-defined version.");
 
 // parse our program's arguments
+// NOTE: this has to come before the flag handling below
 program.parse(process.argv);
+
+// major
+if (program.major) {
+  bumpVersion("major");
+}
+
+// minor
+if (program.minor) {
+  bumpVersion("minor");
+}
+
+// patch
+if (program.patch) {
+  bumpVersion("patch");
+}
+
+// custom (user-defined version)
+if (program.custom) {
+  bumpVersion("custom", program.custom);
+}
