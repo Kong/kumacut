1140e46210d224301d1ffefa091340ab7412fb11
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-10-23 10:28:18 -0400

    added a function for writing the new version to the releases.json file. moved our test files to a different folder for clarity.

--- kuma-version.js
+++ kuma-version.js
@@ -3,6 +3,7 @@
 // required tools
 const fs = require("fs-extra");
 const path = require("path");
+const replace = require("replace-in-file");
 const program = require("commander");
 const chalk = require("chalk");
 const LatestSemver = require("latest-semver");
@@ -12,38 +13,82 @@ const namespace = "Kuma";
 const scriptVer = "0.0.1";
 
 // app version info
-const releases = require("./docs/public/releases.json");
-const latest = LatestSemver(releases);
+const releases = "./targets/releases.json"; // update path when moved to website
+const sidebarNav = "./targets/sidebar-nav.js"; // update path when moved to website
+const latest = LatestSemver(require(releases));
 const sourcVersionDir = path.resolve(__dirname, "./draft");
 
-program.version(scriptVer, "-v, --version", "Output the current version of this script.");
+// this is the token we replace in the documentation
+// markdown files when cutting a new release
+const verToken = "%%VER%%";
 
 /**
- * copyDir
+ * @function replaceVerToken
  *
- * Copy the base version directory to a new one
+ * @description This will search for the version token in our source docs folder
+ * and replace it with the version number specified by our release type
+ */
+replaceVerToken = (token, ver) => {
+  const options = {
+    files: `${ver}/**/*.md`,
+    from: new RegExp(token, "g"),
+    to: ver
+  };
+
+  try {
+    const results = replace.sync(options);
+    console.log(`${chalk.green.bold("✔")} Version number updated to ${chalk.blue.bold(ver)} in all Markdown files!`);
+  } catch (err) {
+    console.log(chalk.red.bold(err));
+  }
+};
+
+/**
+ * @function copyDir
+ *
+ * @description Copy the base version directory to a new one
  */
 copyDir = (source, dest) => {
-  fs.copy(source, dest, err => {
+  fs.copy(source, dest)
+    .then(() => {
+      // let the user know that their folder has been created
+      console.log(`${chalk.green.bold("✔")} New version folder created!`);
+    })
+    .then(() => {
+      // replace the version token in the documentation markdown files accordingly
+      replaceVerToken(verToken, dest.replace("./", ""));
+    })
+    .catch(err => {
+      console.log(chalk.red.bold(err));
+    });
+};
+
+/**
+ * @function updateReleaseList
+ *
+ * @description Updates the release list JSON file to include the new version
+ */
+updateReleaseList = (list, ver) => {
+  const listSrc = require(list);
+  let versions = listSrc;
+
+  // update the release object
+  versions.push(ver);
+
+  // write the new object to the release list
+  fs.writeFileSync(list, JSON.stringify(versions, null, 2), err => {
     if (err) {
       console.log(chalk.red.bold(err));
-    } else {
-      console.log(`${chalk.green.bold("✔")} New version folder created!`);
     }
+
+    console.log(`${chalk.green.bold("✔")} ${chalk.blue.bold(ver)} added to release list file!`);
   });
 };
 
 /**
- * updateReleaseList
+ * @function bumpVersion
  *
- * Updates the release list JSON file to include the new version
- */
-updateReleaseList = (list, ver) => {};
-
-/**
- * bumpVersion
- *
- * Bump the version based on the release type or a custom value.
+ * @description Bump the version based on the release type or a custom value.
  */
 bumpVersion = (type, val) => {
   let currentVer = latest.split(".");
@@ -79,15 +124,21 @@ bumpVersion = (type, val) => {
 
   // create the new version folder accordingly
   copyDir(sourcVersionDir, `./${ver}`);
+
+  // update the release list
+  updateReleaseList(releases, ver);
 };
 
+// all of our program's option and functionality couplings
 program
+  .version(scriptVer, "-v, --version", "Output the current version of this script.")
   .option("--major", "Cut a major release.")
   .option("--minor", "Cut a minor release.")
   .option("--patch", "Cut a patch release.")
   .option("--custom <ver>", "Cut a release with a user-defined version.")
   .option("-l, --latest", `Display the latest version of ${namespace}.`)
   .option("-a, --all", `Display all versions of ${namespace}.`)
+  .option("--sidebar", "Display the current sidebar arrangement.")
   .parse(process.argv);
 
 // get the latest version
@@ -118,4 +169,17 @@ if (program.patch) {
 // custom (user-defined version)
 if (program.custom !== undefined) {
   bumpVersion("custom", program.custom);
+}
+
+if (program.sidebar) {
+  // make sure the sidebar nav file exists first
+  fs.access(sidebarNav, fs.F_OK, err => {
+    if (err) {
+      console.log(chalk.red.bold(err));
+      return false;
+    }
+
+    console.log(chalk.blue.bold("Current sidebar structure:"));
+    console.table(require(sidebarNav));
+  });
 }
